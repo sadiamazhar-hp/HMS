2cbddb2635d24749000346ad1a147fc864442add
sadiamazhar-hp <80788093+sadiamazhar-hp@users.noreply.github.com>
2025-02-13 15:17:31 +0500

    done

---
+++ Models/LoginSignup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Contracts;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace V._3._0.Models
+{
+    public enum UserRoles
+    {
+        Admin,
+        Patient,
+        Doctor,
+        Staff
+
+    }
+    public class Signup
+    {
+        [BindProperty]
+        public int SignupId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set;}
+
+        [Required]
+        [Column(TypeName = "nvarchar(max)")]
+        public string Roles { get; set; }
+        //on return from DB
+        //RolesEnum userRole = Enum.Parse<RolesEnum>(user.Roles);
+
+
+
+        public ICollection<Patients> Patients { get; set; }
+    }
+
+
+    public class Payment : Signup
+    {
+        [Required]
+        public int Package { get; set; }
+
+        [Required]
+        public int CardType { get; set; }
+
+        [Required]
+        public string CardName { get; set; }
+
+        [Required]
+        public int Exp { get; set; }
+
+        [Required]
+        public int Cvc { get; set; }
+    }
+
+
+    
+}
---
+++ Repostories/UserRepo.cs
@@ -0,0 +1,29 @@
+﻿using V._3._0.App_Data;
+using V._3._0.Interfaces;
+using V._3._0.Models;
+
+namespace V._3._0.Repostories
+{
+    public class UserRepo : IUser
+    {
+        public readonly HospitalData db;
+        private List<Signup> Users = new()
+        {
+            new Signup {Name="Saifi",Email="saifi2@gmail.com",Password="3829"},
+            new Signup {Name="Agha",Email="Agha12@gmail.com",Password="1090"}
+        };
+        public Signup GetByUsernameAndPassword(string username, string Email, string password)
+        {
+            var user = Users.FirstOrDefault(u=>
+            u.Name == username && u.Password ==password && u.Email == Email);
+
+            return user;
+        }
+        public Signup? GetByGoogleId()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
